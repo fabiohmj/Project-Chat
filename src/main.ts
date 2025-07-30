885bf07058f709bb561aa1f8a48acc0d85b8574a
fabiohenrique <engenharia18@tkth.com.br>
2025-07-30 14:23:22 -0300

    feat: :sparkles: Add simple chat with socket (back-end)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import type { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  await app.listen(3000);
 }
 bootstrap();
